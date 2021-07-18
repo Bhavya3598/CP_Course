65141bab46d9c4dbc6b3781cbf66b7ef8961b2f2
Bhavya3598 <bhavyasnandikolla@gmail.com>
2021-07-19 00:00:59 +0530

    2020501109_queue.py

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -7,12 +7,23 @@ Make sure you pass the test cases too!"""
 class Queue:
     def __init__(self, head=None):
         self.storage = [head]
+        
+
 
     def enqueue(self, new_element):
-        pass
+        self.storage.append(new_element)
+        return self.storage
+        
 
     def peek(self):
-        pass 
+        return self.storage[0]
 
     def dequeue(self):
-        pass
+        top=self.storage[0]
+        self.storage.remove(top)
+        return top
+
+# queues = Queue(5)
+# print(queues.enqueue(1))
+# print(queues.dequeue())
+# print(queues.peek())
