4b321a5845bf87959efb8eeb6f68f0f8840cc695
Bhavya3598 <bhavyasnandikolla@gmail.com>
2021-07-27 17:51:49 +0530

    2020501109_rotatestring.py

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -6,6 +6,41 @@
 
 
 
-def fun_rotatestrings(s, n):
-	return s
+def fun_rotatestrings(s, k):
+	if(k>=0):
+		for i in range(k):
+			get_ele=s[0]
+			new_s=s[1:]
+			s=new_s+get_ele
+		return s
+	if(k<0):
+		k=abs(k)
+		for i in range(k):
+			get_ele=s[-1]
+			new_s=s[0:-1]
+			s=get_ele+new_s
+		return s
 
+
+
+		# get_org_len=len(s)
+		# req_len=k-get_org_len
+		# for i in s:
+		# 	get_char=s[:req_len]
+		# 	combi=s+get_char
+		# 	output=combi[req_len:]
+		# return output
+		
+
+
+
+	
+
+
+
+# print(fun_rotatestrings("abcd", ))
+
+
+
+# ("abcd", -1, "dabc"), ("abcd", 1,"bcda"),("abcd", -6,"cdab"),
+# ("abcd", 3, "dabc"), ("ac323", 8, "23ac3"),("ac232", 0, "ac232")
