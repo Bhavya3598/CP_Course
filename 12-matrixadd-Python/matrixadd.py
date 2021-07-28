7ea3c740f7206d8da9662ba00fd0eff78c3f6585
Bhavya3598 <bhavyasnandikolla@gmail.com>
2021-07-28 18:02:52 +0530

    2020501109_matrixadd.py

--- 12-matrixadd-Python/matrixadd.py
+++ 12-matrixadd-Python/matrixadd.py
@@ -16,5 +16,24 @@
 # cannot be added because they are of different dimensions.
 
 def matrixadd(L, M):
-	# Your code goes here
-	pass
+	try:
+		result = [[L[i][j] + M[i][j]  for j in range(len(L[0]))] for i in range(len(L))] 
+		return result
+	except IndexError:
+		return None
+
+
+			
+
+
+
+	
+
+# print(matrixadd([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25]]))
+
+
+# ([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25, 26]], [[22, 24, 26],[28, 30, 32]]),
+# ([[1,  2,  3],[4,  5,  6], [7, 8, 9]], [[1,  2,  3],[4,  5,  6], [7, 8, 9]], [[2, 4, 6],[8, 10, 12], [14, 16, 18]]),
+# ([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25]], None),
+# ([[1]], [[10]], [[11]]),
+# ([[1, 2]], [[10]], None)
