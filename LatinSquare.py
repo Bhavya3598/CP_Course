22a2218044ea6fced9871e9f5bbe162a8e21f276
Bhavya3598 <bhavyasnandikolla@gmail.com>
2021-08-02 14:57:45 +0530

    2020501109_latinsquare.py

--- LatinSquare.py
+++ LatinSquare.py
@@ -1,5 +1,5 @@
 # isLatinSquare(a)
-# Write the function isLatinSquare(a) that takes a 2d list 
+# Write the function isLatinSquare(a) that tavares a 2d list 
 # and returns True if it is a Latin square and False otherwise.
 
 # Check for Latin square in the following link:
@@ -7,6 +7,21 @@
 
 # Write your own test cases...
 
-def isLatinSquare(lst):
-    # Your code goes here...
-    pass
+def isLatinSquare(n):
+    var = n + 1
+    for i in range(1, n + 1, 1):
+        temp = var
+        while (temp <= n) :
+            print(temp, end = " ")
+            temp += 1
+        for j in range(1, var):
+            print(j, end = " ")
+        var -= 1
+        print()
+
+
+
+
+
+
+# print(isLatinSquare(5))
