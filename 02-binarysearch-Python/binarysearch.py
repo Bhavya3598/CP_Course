4b57270d3fae2a738402aa2d25965a148477c994
Bhavya3598 <bhavyasnandikolla@gmail.com>
2021-08-03 20:00:56 +0530

    20205011098_binarysearch.py

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -11,17 +11,15 @@ Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
 def binary_search(input_array, value):
-    for i in input_array:
-        # print(i)
-        if(i==value):
-            return input_array.index(i)
-    else:
-        return -1
-
-
-
-
-
-
-
-print(binary_search([1,3,9,11,15,19,29],15))
+    input_array.sort()
+    lo=0
+    hi = len(input_array)-1
+    while lo <= hi:
+        mid = (hi+lo)//2
+        if input_array[mid] == value:
+            return mid
+        elif(value < input_array[mid]):
+            hi = mid-1
+        else:
+            lo=mid+1
+    return -1
